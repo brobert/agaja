a8557f27b4edf29e93bf0efcd1e5ed3d025479cf
brobert <robert.blonski+git@gmail.com>
2016-10-14 22:11:31 +0200

    update footer copyrights

--- resources/views/techedu/layout/partials/footer.blade.php
+++ resources/views/techedu/layout/partials/footer.blade.php
@@ -16,16 +16,7 @@
                 <div class="footer-container">
                     <div class="row">
                         <div class="col-md-6">
-                            <span>&copy; 2016 <a href="#">Power-Boosts</a>. All rights reserved</span>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="social-links">
-                                <a href="#"><i class="fa fa-facebook"></i></a>
-                                <a href="#"><i class="fa fa-google-plus"></i></a>
-                                <a href="#"><i class="fa fa-twitter"></i></a>
-                                <a href="#"><i class="fa fa-pinterest-p"></i></a>
-                                <a href="#"><i class="fa fa-instagram"></i></a>
-                            </div>
+                            <span>&copy; 2016 Robert Błoński</span>
                         </div>
                     </div>
                 </div>
