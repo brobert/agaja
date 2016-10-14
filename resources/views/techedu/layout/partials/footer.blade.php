eaa3e33bacad8446ae39d1ef120f287ed5c62bf6
rb5169 <rb5169@hurra.com>
2016-10-14 15:34:42 +0200

    standarize footer

--- resources/views/techedu/layout/partials/footer.blade.php
+++ resources/views/techedu/layout/partials/footer.blade.php
@@ -11,25 +11,5 @@
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="footer-container">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <span>&copy; 2016 <a href="#">Power-Boosts</a>. All rights reserved</span>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="social-links">
-                                <a href="#"><i class="fa fa-facebook"></i></a>
-                                <a href="#"><i class="fa fa-google-plus"></i></a>
-                                <a href="#"><i class="fa fa-twitter"></i></a>
-                                <a href="#"><i class="fa fa-pinterest-p"></i></a>
-                                <a href="#"><i class="fa fa-instagram"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
